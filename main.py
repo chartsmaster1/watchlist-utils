b97740975006c54ad25b7d7487c62c4dd79d3dc3
HamidM6 <33704063+HamidM6@users.noreply.github.com>
2022-07-17 12:18:40 -0400

    added companies market cap info

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import prep_dow
 import prep_nasdaq
 import prep_sandp_sectors
 import prep_sandp
+import prep_companies_market_cap
 
 
---
+++ prep_companies_market_cap.py
@@ -0,0 +1,43 @@
+# Manually download, rename, and move "CompaniesMarketCap.csv" from https://companiesmarketcap.com/ to "data" folder.
+
+# +
+import json
+import pandas as pd
+import logging
+
+logging.basicConfig(filename='error.log', filemode='w', format='%(levelname)s - %(message)s')
+
+
+# +
+def read_prep_companies_market_cap():
+    
+    df = pd.read_csv('./data/CompaniesMarketCap.csv').sort_values('Rank')
+    
+    file_name = 'companiesMarketCap'
+    file_path = './data/' + file_name + '.json'
+    
+    try:
+        info_list = df.values.tolist()
+        
+        res_list = []
+        for item in info_list:
+            d = {
+                'Rank': item[0],
+                'Name': item[1],
+                'Ticker': item[2],
+                'MarketCap': item[3],
+                'Price': item[4],
+                'Country': item[5]
+            }
+            res_list.append(d)
+
+        with open(file_path, "w") as write_file:
+            json.dump(res_list, write_file)
+
+        print('Companies market cap data read was successfull.')
+
+    except Exception as e:
+        logging.error(str(e))
+        print('Companies market cap data read failed.')
+
+read_prep_companies_market_cap()
